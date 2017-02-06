ec435e1cfbf52f0e37dc8a5abe4953cfc6cc1fea
Chyld Medford <chyld.medford@gmail.com>
2017-02-06 11:23:02 +0530

    sum

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Sum from './components/Sum/Sum';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Class Front</h1>
+        <Sum />
       </div>
     );
   }
