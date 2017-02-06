466e66b5596879bba4f12bfec85ac17867a2292f
Chyld Medford <chyld.medford@gmail.com>
2017-02-06 15:42:29 +0530

    list ii

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -6,7 +6,17 @@ import sinon from 'sinon';
 import List from './List';
 import Box from '../Box/Box';
 
+let students;
+
 describe('List', () => {
+  beforeEach(() => {
+    students = [
+      {id:3, text:"bob", css:"selected"},
+      {id:4, text:"sam", css:"empty"},
+      {id:5, text:"sue", css:"selected"}
+    ];
+  });
+
   it('should render without error', () => {
     const wrapper = shallow(<List />);
     expect(wrapper).to.be.ok;
@@ -23,12 +33,19 @@ describe('List', () => {
   });
 
   it('should render out 3 boxes', () => {
-    const students = [
-      {id:3, text:"bob", css:"selected"},
-      {id:4, text:"sam", css:"empty"},
-      {id:5, text:"sue", css:"selected"}
-    ];
     const wrapper = mount(<List header="Students" items={students} />);
     expect(wrapper.find('.box').length).to.equal(3);
+  });
+
+  it('should display sam in the 2nd box', () => {
+    const wrapper = mount(<List header="Students" items={students} />);
+    expect(wrapper.find(Box).at(1).find('div > div').html()).to.equal('<div data-id="4" class="empty">sam</div>');
+  });
+
+  it('should call fn when 2nd box is clicked', () => {
+    const stub = sinon.stub();
+    const wrapper = mount(<List click={stub} header="Students" items={students} />);
+    wrapper.find(Box).at(1).find('div > div').simulate('click');
+    expect(stub.callCount).to.equal(1);
   });
 });
